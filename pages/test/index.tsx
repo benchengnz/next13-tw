7a8b85074f92ea4ba87d943ae61a5ae39834be03
ben cheng <nz_fox@hotmail.com>
2023-10-23 13:47:52 +1300

    add tailwind style to the test page

--- pages/test/index.tsx
+++ pages/test/index.tsx
@@ -4,16 +4,20 @@ export const Test = () => {
   const [counter, setCounter] = useState(0);
 
   return (
-    <div>
-      <h1>Test page</h1>
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-2xl font-bold mb-6">Test page</h1>
       <button
+        className="border-2 border-blue-500 rounded-lg py-2 px-4
+         text-blue-500 hover:bg-blue-500
+         hover:text-white focus:outline-none focus:ring-2"
         onClick={() => {
           setCounter((prev) => prev + 1);
         }}
       >
         click me
       </button>
-      <p>Counter: {counter}</p>
+
+      <p className="mt-4">Counter: {counter}</p>
     </div>
   );
 };

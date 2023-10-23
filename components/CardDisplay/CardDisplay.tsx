f110d56583dc9fe0ee8b8adc91fb22df1bb8ff91
ben cheng <nz_fox@hotmail.com>
2023-10-23 20:37:51 +1300

    add state to selected card

--- components/CardDisplay/CardDisplay.tsx
+++ components/CardDisplay/CardDisplay.tsx
@@ -1,20 +1,50 @@
+import { useState } from "react";
 import PokerCard from "../PokerCard/PokerCard";
 
+type CardData = {
+  value?: string;
+  icon?: string;
+};
+
 const CardDisplay = () => {
+  const [selectedCard, setSelectedCard] = useState<string | null | undefined>(
+    null
+  );
+  const handleCardClick = (card: CardData) => {
+    // Use both value and iconName to uniquely identify a card
+    setSelectedCard(card.value ?? card.icon);
+  };
+  const isCardSelected = (card: CardData) => {
+    return selectedCard === (card.value ?? card.icon);
+  };
+  const cards: CardData[] = [
+    { value: "?" },
+    { icon: "mug-hot-solid" },
+    { value: "1/2" },
+    { value: "1" },
+    { value: "2" },
+    { value: "3" },
+    { value: "5" },
+    { value: "8" },
+    { value: "13" },
+    { value: "20" },
+    { value: "40" },
+    { value: "100" },
+  ];
+
   return (
     <div className="flex flex-wrap">
-      <PokerCard value="?" />
-      <PokerCard icon="mug-hot-solid" />
-      <PokerCard value="1/2" />
-      <PokerCard value="1" />
-      <PokerCard value="2" />
-      <PokerCard value="3" />
-      <PokerCard value="5" />
-      <PokerCard value="8" />
-      <PokerCard value="13" />
-      <PokerCard value="20" />
-      <PokerCard value="40" />
-      <PokerCard value="100" />
+      {cards.map((card, index) => {
+        return (
+          <PokerCard
+            key={index}
+            isSelected={isCardSelected(card)}
+            value={card.value}
+            icon={card.icon}
+            onClick={() => handleCardClick(card)}
+          />
+        );
+      })}
     </div>
   );
 };
--- components/PokerCard/PokerCard.tsx
+++ components/PokerCard/PokerCard.tsx
@@ -4,11 +4,22 @@ import React from "react";
 export interface PokerCardProps {
   value?: string;
   icon?: string;
+  onClick?: VoidFunction;
+  isSelected?: boolean;
 }
 
-const PokerCard: React.FC<PokerCardProps> = ({ value, icon }) => {
+const PokerCard: React.FC<PokerCardProps> = ({
+  value,
+  icon,
+  onClick,
+  isSelected,
+}) => {
+  const cardStyle = isSelected
+    ? "border-2 border-blue-600 bg-blue-100 rounded-md p-4 m-2 flex justify-center items-center h-32 w-32 cursor-pointer"
+    : "border-2 border-gray-300 bg-white rounded-md p-4 m-2 flex justify-center items-center h-32 w-32 cursor-pointer";
+
   return (
-    <div className="border-2 border-gray-300 bg-white rounded-md p-4 m-2 flex justify-center items-center h-32 w-32">
+    <div className={cardStyle} onClick={onClick}>
       {icon ? (
         <Image src={`/icons/${icon}.svg`} width={30} height={30} alt={icon} />
       ) : (
